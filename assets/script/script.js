31915eb150f0b6a49d365abe63612af756be4f96
Rifars <ramlifr12@gmail.com>
2020-11-28 16:36:22 +0700

    add card1

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,3 +9,33 @@ const hamburgerButton = (items, chenges) => {
     });
 }
 hamburgerButton('.navbar-hamburger','.navbar-items');
+
+let rowLeft = document.querySelector('.left i');
+let rowRight = document.querySelector('.right i');
+let card = Array.from(document.querySelectorAll('.cards .card'));
+let cards = document.querySelector('.cards');
+let number = 0;
+
+cards.addEventListener('click',(e) => {
+    let numb = 0;
+    if(e.target.classList.contains('fa-arrow-circle-right')){
+        numb = number++;
+        numb++;
+
+        if(number > 2){
+            number = 0;
+        }
+        card[number].style.display="inline-block";
+        card[numb-1].style.display="none";
+
+    }else if (e.target.classList.contains('fa-arrow-circle-left')){
+        numb = number--;
+        numb--
+        if(number < 0){
+            number = 2;
+        }
+        card[number].style.display="inline-block"
+        card[numb+1].style.display="none";
+    }
+    // console.log(card[number]);
+});
